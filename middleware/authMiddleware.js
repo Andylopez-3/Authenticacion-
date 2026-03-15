ed78d644fdbaa896707ac8822b1d741a194869c4
Andylopez-3 <andylope948@gmail.com>
2026-03-15 11:16:17 -0300

    feat: add auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET ||  'hello word' ;
+
+//verifica si el usuario esta logueado (soporta sesiones y jwt)
+function requireAuth(req, res, next) {
+    // Primero verificamos si hay una sesión activa
+    if (req.session && req.session.userId) {
+        return next();
+    }
+
+    //si no hay sesión, si manda jwt en el header
+    const authHeader = req.headers['authorization'];
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        try {
+            const decoded = jwt.verify(token, JWT_SECRET);
+            req.user = decoded; // Guardamos los datos del usuario en req.user
+            return next();
+        } catch (err) {
+            return res.status(401).json({ message: 'Token inválido' });
+        }
+    }
+
+    //No tiene ni sesión ni token
+    res.redirect('/login');
+}
+
+module.exports = { requireAuth };
